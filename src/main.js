09816fe411c99bd8b5ab2bed2c1c6b638313a3fa
mohudehei <3189686216@qq.com>
2024-11-03 16:28:25 +0800

    11
    11

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import axiosInstance from './index'
-
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 Vue.prototype.$axios = axiosInstance;
